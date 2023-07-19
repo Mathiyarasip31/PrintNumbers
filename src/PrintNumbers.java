e4e9cc327c0f6ad70e58cb8af8749e1d8a6c2048
Mathiyarasip31 <mathiyarasip31@gmail.com>
2023-07-19 22:07:16 +0530

    Initial commit done

---
+++ src/PrintNumbers.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class PrintNumbers {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int input= scanner.nextInt();
+        Print(input);
+    }
+    public static void Print(int input) {
+        int result = 0;
+        if (input == 1) {
+            System.out.print("1"+" ");
+        } else {
+            Print(input - 1);
+            System.out.print(input+" ");
+        }
+    }
+}
